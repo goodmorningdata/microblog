e4203fa9e5c265667f00890f4f4618d78c2d5ea1
goodmorningdata <goodmorningdata@gmail.com>
2019-09-03 11:36:54 -0500

    Chapter 11

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -11,6 +13,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 '''
 The Flask-Login extension manages the user logged-in state, so that for example users can log in to the application and then navigate to different pages while the application "remembers" that the user is logged in. It also provides the "remember me" functionality that allows users to remain logged in even after closing the browser window. Requires certain properties and methods to be implemented in the user model - is_authenticated, is_active, is_anonymous, get_id(). Generic implementations included in mixin class, UserMixin.
--- config.py
+++ config.py
@@ -14,10 +14,10 @@ class Config(object):
     POSTS_PER_PAGE = 3
 
     # Email server details.
-    # Test mail server is not working - FIX THIS!
-    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    # ADMINS = ['goodmorningdata@gmail.com']
+    #Test mail server is not working - FIX THIS!
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['goodmorningdata@gmail.com']
